b6b0226f13c4ee0e6eb02d22bc518c0ead0de2df
Alex555\Alex <denver55all@hotmail.com>
2020-11-18 13:16:54 +0200

    Added types table-event helper

--- frontend/src/app/shared/helpers/table-event.ts
+++ frontend/src/app/shared/helpers/table-event.ts
@@ -1,6 +1,9 @@
 // Helper utils for table event (click) handlers
+import { NotificationService } from '../services/notification-service';
 
-export function isEventValid($event: any, component: string, notifier: any): boolean {
+export function isEventValid($event: any,
+                             component: string,
+                             notifier: NotificationService): boolean {
   if ($event && typeof $event.value === 'undefined') {
     notifier.showWarn(component, 'No data in event selector!');
     return false;
